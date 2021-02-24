12e6c682f62f9346bc40d63a8d76119d7b6daecb
Nikita <vasrecords1@gmail.com>
2021-02-24 21:41:52 +0300

    to project 1

---
+++ js/script_1.js
@@ -0,0 +1,36 @@
+"use strict";
+
+// Числа
+
+let number = 5;
+
+console.log(4/0);
+console.log('string' * 10);
+
+// Строки
+
+const persone = 'Nikita';
+let famali = 'Anna and Nikita';
+
+// Логика
+
+const bool = true; //false
+
+// Объекты
+
+const obj = {   //Объект
+    name: 'Nikitosina',     //Свойство объекта
+    age: 26,                //Свойство объекта
+    isMarried: false        //Свойство объекта
+};
+
+console.log(obj.name); //Обращение к свойствам объекта через точку
+console.log(obj['age']); //Обращение через скобки, почти не применяется
+
+let arr = ['pineapple.jpg', 'cherry.bmp', 'orange.gif', 32, {}, []];
+console.log(arr[3]);
+
+const razdel = ('sumki');
+console.log(`http//mysite.ru/${razdel}/3`);
+
+alert('hi');
