606abab43b51e698650bf2d7a9f116c10bf30b8f
Nikita <vasrecords1@gmail.com>
2021-03-24 21:25:03 +0300

    zadacha1

--- js/script_1.js
+++ js/script_1.js
@@ -1,36 +1,21 @@
 "use strict";
 
-// Числа
+let number0fFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-let number = 5;
-
-console.log(4/0);
-console.log('string' * 10);
-
-// Строки
-
-const persone = 'Nikita';
-let famali = 'Anna and Nikita';
-
-// Логика
-
-const bool = true; //false
-
-// Объекты
-
-const obj = {   //Объект
-    name: 'Nikitosina',     //Свойство объекта
-    age: 26,                //Свойство объекта
-    isMarried: false        //Свойство объекта
+let personalMovieDB = {
+    count: number0fFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
 };
 
-console.log(obj.name); //Обращение к свойствам объекта через точку
-console.log(obj['age']); //Обращение через скобки, почти не применяется
+let a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его?', ''),
+    c = prompt('Один из последних просмотренных фильмов?', ''),
+    d = prompt('На сколько оцените его?', '');
 
-let arr = ['pineapple.jpg', 'cherry.bmp', 'orange.gif', 32, {}, []];
-console.log(arr[3]);
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
-const razdel = ('sumki');
-console.log(`http//mysite.ru/${razdel}/3`);
-
-alert('hi');
+console.log(personalMovieDB);
