c84e2e69ecea8c0b82f95fe5741d75b668097231
Nikita <vasrecords1@gmail.com>
2021-05-12 18:44:24 +0300

    lol

--- js/script_1.js
+++ js/script_1.js
@@ -19,3 +19,29 @@ personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB);
+
+
+
+const num = 60;
+
+if (num < 59) {
+    console.log('Error');
+} else if (num > 100) {
+    console.log('Дохуя');
+} else {
+    console.log('Самое то');
+}
+
+(num == 60) ? console.log('Самое то') : console.log('Error'); 
+
+switch (num) {
+    case 50: 
+        console.log('Error');
+        break;
+    case 60: 
+        console.log('Огонь');
+        break;
+    default: 
+        console.log('Не сейчас');
+        break;
+}
